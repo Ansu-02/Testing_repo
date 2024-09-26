6b7fbf8d139e54b6ce61673547125bbfe1fe3003
Ansu-02 <angshupyythoon@gmail.com>
2024-09-26 05:10:20 -0700

    Factorial program written in C

---
+++ Factorial.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int facto(int num)
+{
+    if(num == 1 || num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return num*factorial(num-1);
+    }
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number\n");
+    scanf("%d", num);
+    int result = facto(num);
+    printf("The factorial of %d is %d\n", num, result);
+    return 0;
+}
